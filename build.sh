310c277dc0555044ecfed97a7707c2aee39d1df0
Kaito Kishi <17728957+TRSasasusu@users.noreply.github.com>
2026-02-14 23:24:07 +0900

    make directory

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp The\ Stranger.per /mnt/c/Program\ Files\ \(x86\)/Steam/steamapps/common/AoE2DE/resources/_common/ai/.
+cp -r The\ Stranger /mnt/c/Program\ Files\ \(x86\)/Steam/steamapps/common/AoE2DE/resources/_common/ai/.
