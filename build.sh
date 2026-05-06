beb1d539637cc221d65d23f945d2d2f1e0b76265
Kaito Kishi <17728957+TRSasasusu@users.noreply.github.com>
2026-05-06 18:55:00 +0900

    make no wall version

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+if [ ! -e /mnt/c/Program\ Files\ \(x86\)/Steam/steamapps/common/AoE2DE/resources/_common/ai/The\ Stranger.ai ]; then
+    echo "make The\ Stranger.ai"
+    cp The\ Stranger.ai /mnt/c/Program\ Files\ \(x86\)/Steam/steamapps/common/AoE2DE/resources/_common/ai/.
+fi
 cp The\ Stranger.per /mnt/c/Program\ Files\ \(x86\)/Steam/steamapps/common/AoE2DE/resources/_common/ai/.
 cp -r The\ Stranger /mnt/c/Program\ Files\ \(x86\)/Steam/steamapps/common/AoE2DE/resources/_common/ai/.
+
+if [ ! -e /mnt/c/Program\ Files\ \(x86\)/Steam/steamapps/common/AoE2DE/resources/_common/ai/The\ Stranger\ \(no\ wall\).ai ]; then
+    echo "make The\ Stranger\ \(no\ wall\).ai"
+    cp The\ Stranger\ \(no\ wall\).ai /mnt/c/Program\ Files\ \(x86\)/Steam/steamapps/common/AoE2DE/resources/_common/ai/.
+fi
+cp The\ Stranger\ \(no\ wall\).per /mnt/c/Program\ Files\ \(x86\)/Steam/steamapps/common/AoE2DE/resources/_common/ai/.
